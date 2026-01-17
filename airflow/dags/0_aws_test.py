f2e011a55fd7332b5df7e6beef5e7419f5ed57e0
Joan <joanstudyai@gmail.com>
2026-01-17 12:40:56 +0100

    fix: validation schema for testing

--- airflow/dags/0_aws_test.py
+++ airflow/dags/0_aws_test.py
@@ -1,7 +1,7 @@
 from airflow.sdk import dag
 from pendulum import datetime
 from utils_db import run_batch_sql
-
+ 
 @dag(
     dag_id="0_aws_batch_test",
     start_date=datetime(2023, 1, 1),
@@ -10,17 +10,18 @@ from utils_db import run_batch_sql
     tags=['test', 'local']
 )
 def aws_batch_test():
-
+ 
     test_query = """
-        CREATE TABLE IF NOT EXISTS gold.test_on_aws AS (SELECT 1 as val);
-        DROP TABLE gold.test_on_aws;
+        CREATE SCHEMA IF NOT EXISTS verification;
+        CREATE TABLE IF NOT EXISTS verification.test_on_aws AS (SELECT 1 as val);
+        DROP TABLE verification.test_on_aws;
     """
-
+ 
     task_test_sql = run_batch_sql(
         task_id='test_aws_sql_connection',
         sql_query=test_query,
         memory="2GB",
         vcpu=1
     )
-
+ 
 aws_batch_test()
